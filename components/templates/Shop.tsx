72e315faa306e3ce58760081666ca8ea0f7f5234
Jean-Baptiste Terrazzoni <jterrazz@icloud.com>
2020-05-21 18:12:04 +0200

    save

--- components/templates/Shop.tsx
+++ components/templates/Shop.tsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import StoreBanner from '../organisms/banner/StoreBanner';
 import SectionTags from '../molecules/SectionTags';
 import ProductCellsGrid from '../organisms/grid/ProductCells';
-import { TOMObject, TOMRouter } from '../../librairies/tom-classes/index';
+import { TOMObject, TOMRouter } from '@tom';
 
 const Shop = ({ shopInfos, defaultProducts, getProducts }) => {
     const bannerInfos = TOMObject.mapKeys(shopInfos, { name: 'title' });
