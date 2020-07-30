7fac8eefbac79e4ef8255540932160c0c4448fe2
Jean-Baptiste Terrazzoni <jterrazz@icloud.com>
2020-07-30 21:07:23 +0200

    updates

--- src/components/organisms/information/MSFeatures.tsx
+++ src/components/organisms/information/MSFeatures.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const MSFeatures = () => (
-    <div className='w-100 bg-primary text-white py-5'>
+    <div className='w-100 py-5'>
         <div className='container'>
             <h3>Sell with us and without us</h3>
             <div className='bg-white rounded'>Hello</div>
--- src/components/templates/Home.tsx
+++ src/components/templates/Home.tsx
@@ -3,13 +3,48 @@ import Link from 'next/link';
 
 import MSFeatures from '../organisms/information/MSFeatures';
 
+const url = '/images/bg/pawel-czerwinski-TTBEwfajRFc-unsplash.jpg';
+
+const bgStyle = (url) => {
+    return {
+        background: `url(${url})`,
+        'background-repeat': `no-repeat`,
+        'background-size': `cover`,
+        height: 400,
+    };
+};
+
 const Home = () => (
     <main>
-        <div className='my-5' />
+        <div className='mb-2' />
+
+        <div
+            style={bgStyle(url)}
+            className='w-100 p-5 text-white text-center d-flex flex-column justify-content-center'
+        >
+            <h1>Start selling now</h1>
+        </div>
+
+        <div className='d-flex text-center m-2'>
+            <div className='flex-fill rounded' style={{ backgroundColor: 'rgb(223, 234, 242)' }}>
+                <h2>Mes commandes</h2>
+                <p>3 en cours</p>
+            </div>
+            <div className='ml-2 flex-fill rounded' style={{ backgroundColor: 'rgb(238, 223, 215)' }}>
+                <h2>Mes commandes</h2>
+                <p>3 en cours</p>
+            </div>
+            <div className='ml-2 flex-fill rounded'>
+                <h2>Mes commandes</h2>
+                <p>3 en cours</p>
+            </div>
+        </div>
+
         <Link href='/shops/[id]' as={`/shops/${2}`}>
-            tset
+            <a>Shop</a>
         </Link>
         <MSFeatures />
+        <div>Welcome back Jean-Baptiste</div>
     </main>
 );
 
