e374097305aedf105a04b1c0537fe12124a5357b
Terrazzoni <jterrazz@icloud.com>
2020-11-07 20:59:44 +0100

    APP-1 - SEO improvements and new home page message

--- src/components/molecules/messages/People.tsx
+++ src/components/molecules/messages/People.tsx
@@ -1,1 +1,7 @@
-The first marketplace for people
+import React from 'react';
+
+export const InDevelopmentMessage = () => (
+    <main className='bg-gray--light rounded p-4'>
+        <p>The first marketplace for people</p>
+    </main>
+);
